78691e1e315b14a380143ac32c8efa8dd5ac0386
ValentinaNovarino <valenovarino@gmail.com>
2020-10-14 16:50:13 +0200

    esercizio finito

--- main.js
+++ main.js
@@ -3,17 +3,20 @@
 // creare un array con un insieme di indirizzi email
 var email = ['homer@simpson.com', 'bart@simpson.com', 'marge@simpson.com', 'lisa@simpson.com', 'ned@flanders.com', 'boe@szyslak.com' ];
 
-// console.log(email)
 
 // chiedere all'utente il suo indirizzo email
 var emailUtente = prompt('Inserisci la tua email')
-// console.log(emailUtente);
+var utenteRegistrato = false;
 
 // verificare se è nella lista (tramite un ciclo for) e comunicare un messaggio adeguato a seconda del risultato
 for (var i = 0; i < 6; i++) {
     if (email[i] == emailUtente) {
-        console.log('Congratulazioni sei nella lista!')
-    } else {
-        console.log('Spiacenti, non sei nella lista.');
+        utenteRegistrato = true;
     }
+}
+
+if (utenteRegistrato) {
+    document.getElementById('user').innerHTML = 'Congratulazioni sei nella lista!';
+} else {
+    document.getElementById('user').innerHTML = 'Spiacenti non sei nella lista.';
 }
