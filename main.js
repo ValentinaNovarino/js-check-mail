1092cad8e712e7a41c0011fa292412ca6ed1384a
ValentinaNovarino <valenovarino@gmail.com>
2020-10-14 15:23:56 +0200

    creato array

---
+++ main.js
@@ -0,0 +1,11 @@
+// Esercitazione 1: Check lista email. Creare un array con un insieme di indirizzi email. Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for). Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
+
+// creare un array con un insieme di indirizzi email
+var email = ['homer@simpson.com', 'bart@simpson.com', 'marge@simpson.com', 'lisa@simpson.com', 'ned@flanders.com', 'boe@szyslak.com' ];
+
+console.log(email)
+
+// chiedere all'utente il suo indirizzo email
+
+
+// verificare se è nella lista (tramite un ciclo for) e comunicare un messaggio adeguato a seconda del risultato
